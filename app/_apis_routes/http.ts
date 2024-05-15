83831baec2dc3d1f2feff92c9aac82e6efb82862
Dipta Biswas <dipta.biswas231998@gmail.com>
2024-05-15 18:24:53 +0530

    updated

--- app/_apis_routes/http.ts
+++ app/_apis_routes/http.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
+export const axiosInstance = (() => axios.create({
     baseURL: "http://localhost:3000/api",
     timeout: 30 * 1000,
     withCredentials: true,
     withXSRFToken: true,
-})
+}))()
