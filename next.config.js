f1015a73789bea80176b820ebcf6a0ab52622aeb
Dipta Biswas <dipta.biswas231998@gmail.com>
2024-06-11 14:29:37 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,37 +1,41 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
-const { merge } = require('webpack-merge');
+const path = require("path");
+const { merge } = require("webpack-merge");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "images.pexels.com",
-			},
-		],
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
 
-	},
-
-	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-		if (isServer && config.name === 'server') {
-			return merge(config, {
-				entry() {
-					return config.entry().then((entry) => {
-						return Object.assign({}, entry, {
-							"update_access.worker": path.resolve(
-								process.cwd(),
-								"workers/update_access.worker.ts"
-							),
-						});
-					});
-				},
-			});
-		} else {
-			return config;
-		}
-	},
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (isServer && config.name === "server") {
+      return merge(config, {
+        entry() {
+          return config.entry().then((entry) => {
+            return Object.assign({}, entry, {
+              "update_access.worker": path.resolve(
+                process.cwd(),
+                "workers/update_access.worker.ts"
+              ),
+            });
+          });
+        },
+      });
+    } else {
+      return config;
+    }
+  },
 };
 
 module.exports = nextConfig;
