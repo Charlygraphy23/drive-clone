4974b714b9041a0d5f2f463d5d27d7ebb40c394e
Dipta Biswas <46165735+Charlygraphy23@users.noreply.github.com>
2024-07-06 12:54:09 +0530

    updated

--- app/_apis_routes/http.ts
+++ app/_apis_routes/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = (() => axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "/api",
     timeout: 30 * 1000,
     withCredentials: true,
     withXSRFToken: true,
--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require("path");
+// const path = require("path");
 // const { merge } = require("webpack-merge");
 
 /** @type {import('next').NextConfig} */
