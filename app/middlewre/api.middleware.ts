f97cfd54f9f39e39222ba2f699ce419aed79e5f8
Dipta Biswas <dipta.biswas231998@gmail.com>
2024-08-27 15:49:11 +0530

    updated

--- app/middlewre/api.middleware.ts
+++ app/middlewre/api.middleware.ts
@@ -1,7 +1,7 @@
 import { JWT } from "next-auth";
 import { NextRequest } from "next/server";
 
-const publicPaths = ["/signup", "/jobs", "/path", "/plans"]
+const publicPaths = ["/signup", "/jobs", "/path", "/plans", "/ping"]
 
 export const checkAuthForApiRoute = async (request: NextRequest, token: JWT | null) => {
     const url = new URL(request.url);
