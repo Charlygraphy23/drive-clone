7c122f4a7ef0c5326cd0b905944527b23fe5bbbe
Dipta Biswas <46165735+Charlygraphy23@users.noreply.github.com>
2024-08-31 13:48:03 +0530

    upated

--- app/components/LocalImage/index.tsx
+++ app/components/LocalImage/index.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import Image from 'next/image';
-import React from 'react';
+import { useAppSelector } from '@/app/store';
+import Image, { ImageLoader } from 'next/image';
+import React, { useCallback } from 'react';
 
 type Props = {
     style?: React.CSSProperties;
@@ -20,17 +21,17 @@ type Props = {
 }
 
 const LocalImage = ({ style, fill, alt, sizes, priority, onLoad, src, placeholder, loading, blurDataURL, className }: Props) => {
-    // const { type } = useAppSelector(state => state.network)
-    // const imageLoader: ImageLoader = useCallback(({ src, width }) => {
-    //     const origin = window.location.origin
-    //     return `${origin}/${src}?w=${width}&quality=${type}`
-    // }, [])
+    const { type } = useAppSelector(state => state.network)
+    const imageLoader: ImageLoader = useCallback(({ src, width }) => {
+        const origin = window.location.origin
+        return `${origin}/${src}?w=${width}&quality=${type}`
+    }, [type])
 
     return (
         <Image
             className={className}
             src={src}
-            // loader={imageLoader}
+            loader={imageLoader}
             style={style}
             fill={fill}
             alt={alt}
