591032d670f61e06d7bc933172b6f81b9c3cd2a0
Dipta Biswas <46165735+Charlygraphy23@users.noreply.github.com>
2024-09-15 15:19:25 +0530

    updated

--- app/manifest.ts
+++ app/manifest.ts
@@ -38,22 +38,60 @@ export default function manifest(): MetadataRoute.Manifest {
 	return {
 		name: 'MBOX',
 		short_name: 'MBOX',
-		description: 'MBOX',
-		start_url: '/',
+		description: 'A file storage app',
+		start_url: '.',
 		display: 'standalone',
 		background_color: '#fff',
-		theme_color: '#fff',
+		theme_color: '#6a29ff',
 		icons: [
 			{
-				"src": "/android-chrome-192x192.png",
-				"sizes": "192x192",
-				"type": "image/png"
+				"src": "/assets/favicon-16x16.png",
+				"sizes": "16x16",
+				"type": "image/png",
+				"purpose": "any"
 			},
 			{
-				"src": "/android-chrome-384x384.png",
+				"src": "/assets/favicon-32x32.png",
+				"sizes": "32x32",
+				"type": "image/png",
+				"purpose": "any"
+			},
+			{
+				"src": "/assets/android-chrome-192x192.png",
+				"sizes": "192x192",
+				"type": "image/png",
+				"purpose": "any"
+			},
+			{
+				"src": "/assets/android-chrome-384x384.png",
 				"sizes": "384x384",
-				"type": "image/png"
-			}
+				"type": "image/png",
+				"purpose": "any"
+			},
+
 		],
+		"screenshots": [
+			{
+				"src": "https://github.com/user-attachments/assets/97213643-42ed-461c-8bff-4dbf41039431",
+				"sizes": "3360x1786",
+				"type": "image/png",
+				"description": "A file storage app",
+				"form_factor": "wide"
+			},
+			{
+				"src": "https://github.com/user-attachments/assets/1b86563b-8ec8-4c9b-bbac-e90aa7aecec0",
+				"sizes": "3360x1786",
+				"type": "image/png",
+				"description": "A file storage app",
+				"form_factor": "wide"
+			},
+			{
+				"src": "https://github.com/user-attachments/assets/45c8a158-88ca-4476-85d5-55cb861d5a40",
+				"sizes": "782x1684",
+				"type": "image/png",
+				"description": "A file storage app",
+				"form_factor": "narrow"
+			}
+		]
 	}
 }
